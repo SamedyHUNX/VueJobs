accb80cede9884815c3fe91b49b0ae3e9ee46b82
samedyhunx <samedy.hunx@gmail.com>
2026-01-07 15:12:54 +0700

    edit job page for the app

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import JobsView from "@/views/JobsView.vue";
 import NotFound from "@/views/NotFound.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,7 @@ const router = createRouter({
     { path: "/jobs/:id", name: "job", component: JobView },
     { path: "/jobs/add", name: "add-job", component: AddJobView },
     { path: "/:catchAll(.*)", name: "not-found", component: NotFound },
+    { path: "/jobs/edit/:id", name: "edit-job", component: EditJobView },
   ],
 });
 
