1db02305c3104c18eecdc59121382a11b4659d12
samedyhunx <samedy.hunx@gmail.com>
2026-01-07 13:42:22 +0700

    routerlink

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,7 @@
 <script setup>
 import jobData from "@/jobs.json";
 import JobListing from "./JobListing.vue";
+import { RouterLink } from "vue-router";
 import { ref } from "vue";
 
 // reactive jobs
@@ -36,10 +37,10 @@ const props = defineProps({
   </section>
 
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
+    <RouterLink
+      to="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
+      >View All Jobs</RouterLink
     >
   </section>
 </template>
